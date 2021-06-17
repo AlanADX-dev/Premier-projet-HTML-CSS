5b323b3f58465cd516b5c8b7961d0189b230456a
AlanADX <alan.andrieux@gmail.com>
2021-06-17 11:25:15 +0200

    carousel stable et responsive

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-document.getElementsByClassName('navbar-brand')[0].style.display = 'none';
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+      $('#back2Top').fadeIn();
+  } else {
+      $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
 
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 250) {
-          document.getElementById('sticky-nav').classList.add('fixed-top');
-          document.getElementsByClassName('navbar-brand')[0].style.display = 'block';
-          // add padding top to show content behind navbar
-          navbar_height = document.querySelector('.navbar').offsetHeight;
-          document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-          document.getElementById('sticky-nav').classList.remove('fixed-top');
-          document.getElementsByClassName('navbar-brand')[0].style.display = 'none';
-           // remove padding top from body
-          document.body.style.paddingTop = '0';
-        } 
-    });
-  }); 
+});
+/*Scroll to top when arrow up clicked END*/
